5e7acef5e17a533dc88f74664880869dc6e378c3
Ilias J. Kavalis <8596938+Ilias-kav@users.noreply.github.com>
2020-04-16 18:59:53 +0300

    started genetic algorithm implementation

--- WHPP_Genetic_Algorithm/COMP417/WHPP/GA/GeneticAlgorithm.java
+++ WHPP_Genetic_Algorithm/COMP417/WHPP/GA/GeneticAlgorithm.java
@@ -5,5 +5,16 @@ public class GeneticAlgorithm{
 	public static void main(String args[]){
 		Population pop = new Population();
 		//pop.printPop();
+		while(true){
+			if(pop.evaluate()){
+				pop.printBest();
+				break;
+			}
+			else{
+				pop.select();
+				pop.crossbreed();
+				pop.mutate();
+			}
+		}
 	}
 }
--- WHPP_Genetic_Algorithm/COMP417/WHPP/GA/Population.java
+++ WHPP_Genetic_Algorithm/COMP417/WHPP/GA/Population.java
@@ -20,4 +20,33 @@ public class Population {
 		}
 	}
 	
+	public void printBest(){}
+	
+	public void select(){}
+	
+	public void crossbreed(){}
+	
+	public void mutate(){}
+	
+	public boolean evaluate(){
+		assignEvaluation();
+		return true;
+	}
+	
+	private void assignEvaluation(Schedule sched){
+		for(int emp = 0; emp < sched.length; emp++){
+			for(int day = 0; day < sched[emp].length; day++){
+				if(sched[emp][day] == 1 || sched[emp][day] == 2)
+					hours += 8;
+				else if(sched[emp][day] == 3)
+					hours += 10;
+				if(sched[emp][day] != 0)
+					conc++;
+				if(sched[emp][day] == 3)
+					nshift++;
+				
+			}
+		}
+	}
+	
 }
