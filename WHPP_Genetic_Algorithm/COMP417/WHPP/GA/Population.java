8f4aeed596a364312f2cf1cc20db05504fdbd782
Ilias J. Kavalis <8596938+Ilias-kav@users.noreply.github.com>
2020-04-18 18:56:48 +0300

    finished implementing evaluation function

--- WHPP_Genetic_Algorithm/COMP417/WHPP/GA/Population.java
+++ WHPP_Genetic_Algorithm/COMP417/WHPP/GA/Population.java
@@ -29,24 +29,71 @@ public class Population {
 	public void mutate(){}
 	
 	public boolean evaluate(){
-		assignEvaluation();
+		for(int iter = 0; iter < POP_SIZE; iter++){
+			assignEvaluation(pop[iter].sched, iter);
+		}
+		
 		return true;
 	}
 	
-	private void assignEvaluation(Schedule sched){
+	private void assignEvaluation(int[][] sched, int number){
+		int penalty = 0;
 		for(int emp = 0; emp < sched.length; emp++){
+			int free = 0;
+			int conc = 0;
+			boolean needrest = false;
+			int hours = 0;
+			int nshift = 0;
 			for(int day = 0; day < sched[emp].length; day++){
-				if(sched[emp][day] == 1 || sched[emp][day] == 2)
+				if(sched[emp][day] == 1 || sched[emp][day] == 2){
+					if(free == 1)
+						penalty += 1;
+					if(needrest)
+						penalty += 100;
 					hours += 8;
-				else if(sched[emp][day] == 3)
-					hours += 10;
-				if(sched[emp][day] != 0)
+					free = 0;
 					conc++;
-				if(sched[emp][day] == 3)
+					if(conc == 7)
+						needrest = true;
+					nshift = 0;
+					if((day - 1) >= 0 && sched[emp][day] == 1 && sched[emp][day - 1] == 3)
+						penalty += 1000;
+					if((day - 1) >= 0 && sched[emp][day] == 1 && sched[emp][day - 1] == 2)
+						penalty += 800;
+					if((day - 1) >= 0 && sched[emp][day] == 2 && sched[emp][day - 1] == 3)
+						penalty += 800;
+				}
+				else if(sched[emp][day] == 3){
+					if(free == 1)
+						penalty += 1;
+					if(needrest)
+						penalty += 100;
+					hours += 10;
+					free = 0;
+					conc++;
+					if(conc == 7)
+						needrest = true;
 					nshift++;
-				
+					if(nshift == 4)
+						needrest = true;
+					if(nshift > 4)
+						penalty += 1000;
+				}
+				else{
+					conc = 0;
+					free++;
+					if((day - 2) >= 0 && free == 1 && sched[emp][day - 2]!= 0)
+						penalty += 1;
+					if(free >= 2)
+						needrest = false;
+				}
 			}
+			if(hours > 70)
+				penalty += 1000;
+			if(conc > 7)
+				penalty += 1000;
 		}
+		System.out.println("Schedule number "+ number +" penalty: "+ penalty);
 	}
 	
 }
